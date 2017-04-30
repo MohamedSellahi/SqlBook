7a5cf7b177477daa0f87ffd96f0d5cc4aa0fa421
MohamedSellahi <mohamed.sellahi@gmail.com>
2017-04-30 11:25:01 +0200

    end natutal joins

--- db-book/ch3_QueryOperationScripts.sql
+++ db-book/ch3_QueryOperationScripts.sql
@@ -7,14 +7,15 @@ select * from classroom;
 /* instructor relation */
 select * from instructor;
 
-/* section relation */
-select * from section;
-
 /* teaches relation */
 select * from teaches;
 
 /* courses */
 select * from course;
+
+/* section relation */
+select * from section;
+
 
 /* prereq */
 select * from prereq;
@@ -88,5 +89,58 @@ where instructor.ID = teaches.ID and instructor.dept_name = "Comp.Sci."
 /*
 * 3.3.3 The natural join 
 **************************************/
+select name, course_id 
+from instructor, teaches
+where instructor.ID = teaches.ID
+;
+
+/* using the natural join */
+select name, course_id
+from instructor natural join teaches
+;
+
+/* getting all the information about teachers who gives somes
+courses */
+select*
+from instructor natural join teaches
+;
+
+/* List names of instructors along with titles of 
+corses thet teach */
+select name, title
+from instructor natural join teaches, course
+where teaches.course_id = course.course_id
+;
+
+/*compare with this query */
+/* this query will give the same results as the previous one
+while omitting the all (instructor name, course title) where 
+the instructor teaches a course in a department other than 
+his own department*/
+select name, title
+from instructor natural join teaches natural join course
+;
+
+/*using joins while specifying exactly the columns that 
+should be equated */
+select name, title
+from (instructor natural join teaches) join course using(course_id)
+;
+
+/*
+* 3.4.1 the rename operation 
+****************************/
+
+
+
+
+
+
+
+
+
+
+
+
 
 
